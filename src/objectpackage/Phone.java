b6a56fc81b5620809d3270e11d25420af55f9bf7
kostabkpark <kostabkpark@gmail.com>
2024-08-14 17:26:13 +0900

    OOP - class - 캡슐화

--- src/objectpackage/Phone.java
+++ src/objectpackage/Phone.java
@@ -2,10 +2,26 @@ package objectpackage;
 
 class Phone {
   // 필드
-  String model;
-  int price;
+  private String model;
+  private int price;
   // 메서드
   void print() {
     System.out.println(price + "원의 모델 " + model + " 휴대폰입니다.");
+  }
+
+  public String getModel(){
+    return model;
+  }
+
+  public void setModel(String modelname){
+    model = modelname;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
   }
 }
--- src/objectpackage/PhoneDemo.java
+++ src/objectpackage/PhoneDemo.java
@@ -3,12 +3,13 @@ package objectpackage;
 public class PhoneDemo {
   public static void main(String[] args) {
     Phone p1 = new Phone();
-    p1.model = "갤럭시 Z 폴드 6";
-    p1.price = 2_586_000;
+    p1.setModel("갤럭시 Z 폴드 6");
+    p1.setPrice(2_586_000);
     p1.print();
+
     Phone p2 = new Phone();
-    p2.model = "iPhone 15 Pro";
-    p2.price = 2_320_000;
+    p2.setModel("iPhone 15 Pro");
+    p2.setPrice(2_320_000);
     p2.print();
   }
 }
