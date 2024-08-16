0c7d12fe5e19f2bc46b50b94b8b08e5ac9e14dc8
kostabkpark <kostabkpark@gmail.com>
2024-08-16 09:54:30 +0900

    class - capsule 2

---
+++ src/objectpackage/Circle.java
@@ -0,0 +1,40 @@
+package objectpackage;
+
+class Circle {
+  // 필드 - 캡슐화 전
+  private int radius = 1;
+  // 생성자 (기본생성자,~~~)
+//  public Circle(){
+//
+//  }
+  // 메서드
+  /**
+   * 원의 반지름을 출력하는 메서드
+   * 인스턴스를 생성해야 호출할 수 있는 메서드
+   * 매개변수X 리턴값X
+   */
+  public void printRadius(){
+    System.out.println("원의 반지름 : " + radius);
+  }
+  /**
+   * 원의 넓이를 구하여 반환하는 메서드
+   * 매개변수 X 리턴값 O (double)
+   */
+  public double getArea() {
+    final double PI = 3.14;
+    double area = PI * radius * radius;
+    return area;
+  }
+
+  public int getRadius() {
+    return radius;
+  }
+
+  public void setRadius(int radius) {
+    if(radius <= 0){
+      System.out.println("원의 반지름은 0보다 커야 합니다.");
+    } else {
+      this.radius = radius;
+    }
+  }
+}
---
+++ src/objectpackage/CircleDemo.java
@@ -0,0 +1,15 @@
+package objectpackage;
+
+public class CircleDemo {
+  public static void main(String[] args) {
+    Circle c1 = new Circle();
+    int radius = c1.getRadius();
+    double area ;
+    System.out.println("원의 반지름 초기값 : " + radius);
+    c1.setRadius(-1);
+    System.out.println("원의 반지름 변경값 : " + c1.getRadius());
+    c1.printRadius();
+    area = c1.getArea();
+    System.out.println("원의 넓이 : " + area);
+  }
+}
