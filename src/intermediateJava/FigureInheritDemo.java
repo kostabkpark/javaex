cb15834786876bd10aa2d075de65d1c444b5c546
kostabkpark <kostabkpark@gmail.com>
2024-08-21 14:58:23 +0900

    class inheritance - 2

---
+++ src/intermediateJava/FigureInheritDemo.java
@@ -0,0 +1,19 @@
+package intermediateJava;
+
+public class FigureInheritDemo {
+  public static void main(String[] args) {
+
+  }
+}
+
+class Figure {
+  private int poly ;
+  public double getArea(){
+    return 0.0;
+  }
+}
+
+class Triangle extends Figure {
+  private int poly;
+
+}
---
+++ src/intermediateJava/StaticImportDemo.java
@@ -0,0 +1,12 @@
+package intermediateJava;
+
+import java.util.Calendar;
+
+import static java.util.Calendar.getInstance;
+
+
+public class StaticImportDemo {
+  public static void main(String[] args) {
+    getInstance();
+  }
+}
