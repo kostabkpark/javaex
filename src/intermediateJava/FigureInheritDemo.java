fb116192110822a6fa0d94e26cf07e22023d4bef
kostabkpark <kostabkpark@gmail.com>
2024-08-21 17:49:17 +0900

    class upcasting - 1

--- src/intermediateJava/FigureInheritDemo.java
+++ src/intermediateJava/FigureInheritDemo.java
@@ -2,18 +2,55 @@ package intermediateJava;
 
 public class FigureInheritDemo {
   public static void main(String[] args) {
+    Triangle triangle = new Triangle(3,3, 3);
+    double area = triangle.getArea();
+    System.out.println(area);
 
+    RectAngle rectAngle = new RectAngle(4,3,4);
+    area = rectAngle.getArea();
+    System.out.println(area);
   }
 }
 
 class Figure {
-  private int poly ;
+  int poly ;
+
   public double getArea(){
     return 0.0;
   }
 }
 
 class Triangle extends Figure {
-  private int poly;
+  private int height;
+  private int width;
 
+  public Triangle(int poly, int height, int width) {
+    this.height = height;
+    this.width = width;
+  }
+
+//  @Override
+//  public double getArea() {
+//    return super.getArea();
+//  }
+
+  @Override
+  public double getArea() {
+    return (width * height) / (double) 2;
+  }
+}
+
+class RectAngle extends Figure {
+  private int height ;
+  private int width ;
+
+  public RectAngle(int poly, int height, int width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  @Override
+  public double getArea() {
+    return width * height;
+  }
 }
---
+++ src/intermediateJava/SuperConstructorDemo.java
@@ -0,0 +1,22 @@
+package intermediateJava;
+
+public class SuperConstructorDemo {
+  public static void main(String[] args) {
+    ColoredBox cb = new ColoredBox(10);
+    System.out.println(cb.size);
+  }
+}
+
+class Box {
+  int size;
+  //public Box() {}
+  public Box(int size){
+    this.size = size;
+  }
+}
+
+class ColoredBox extends Box {
+  public ColoredBox(int size) {
+    super(size);
+  }
+}
