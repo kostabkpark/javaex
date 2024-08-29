7434464cbf48214277b797645026ee527a61bf93
kostabkpark <kostabkpark@gmail.com>
2024-08-29 10:55:31 +0900

    stream 1

---
+++ src/advancedJava2/Util.java
@@ -0,0 +1,11 @@
+package advancedJava2;
+
+public class Util {
+  public static <T> void print(T t) {
+    System.out.print(t + " ");
+  }
+
+  public static <T> void printWithParenthesis(T t) {
+    System.out.print("(" + t + ") ");
+  }
+}
