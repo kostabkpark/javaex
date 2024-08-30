fce6b3e24708bc2335d1becfa70e086438f989f6
kostabkpark <kostabkpark@gmail.com>
2024-08-30 15:06:55 +0900

    datasturcture slinkedlist removeLast removeFirst

--- src/datastructure/SLinkedList.java
+++ src/datastructure/SLinkedList.java
@@ -2,15 +2,29 @@ package datastructure;
 
 public class SLinkedList<E> {
   private Node<E> head;
+  private Node<E> tail;
   private int size;
 
   public SLinkedList() {
-    head = null;
+    head = tail = null;
     size = 0;
   }
   // 탐색, 삽입, 삭제 , size 를 구하는 메서드 구현
   int size() {
     return size;
+  }
+
+  void addLast(E e){
+    Node<E> newNode = new Node<>();
+    newNode.e = e;
+    newNode.next = tail;
+    if(size == 0) { // head == tail && head == null
+      head = newNode;
+    }
+    tail = newNode;
+    System.out.println(tail.e);
+    System.out.println(tail.next);
+    size++;
   }
 
   void addFirst(E e){
@@ -19,11 +33,55 @@ public class SLinkedList<E> {
     newNode.e = e;
     newNode.next = head;
     // 2단계
+    if(size == 0) {
+      tail = newNode;
+    }
     head = newNode;
     // 3단계
     size++;
   }
 
+  E getValue(int index){
+    Node<E> find = head;
+    for (int i = 0; i < index; i++) {
+      find = find.next;
+    }
+    return find.e;
+  }
+
+  Node<E> getNode(int index){
+    Node<E> find = head;
+    System.out.println(find.e);
+    for (int i = 0; i < index; i++) {
+      System.out.println(find.e);
+      find = find.next;
+    }
+    return find;
+  }
+
+  E removeLast() {
+    Node<E> removedNode = tail;
+    if(size>1) {
+      Node<E> previousNode = getNode(size - 2);
+      previousNode.next = null;
+      tail = previousNode;
+    } else if (size == 1) {
+      head = tail = null;
+    }
+    size--;
+    return removedNode.e;
+  }
+
+  E removeFirst() {
+    Node<E> removedNode = head;
+    head = head.next;
+    removedNode.next = null;
+    if (size == 1) {
+      head = tail = null;
+    }
+    size--;
+    return removedNode.e;
+  }
 
   private class Node<E> {
     E e;
--- src/datastructure/SLinkedListDemo.java
+++ src/datastructure/SLinkedListDemo.java
@@ -7,12 +7,27 @@ public class SLinkedListDemo {
     LinkedList<Integer> ll = new LinkedList<>();
     System.out.println(ll.size());
     ll.addFirst(10);
+    Integer i = ll.removeLast();
+    System.out.println(i + "이 삭제되었습니다.");
+
     System.out.println(ll.size());
     System.out.println(ll);
 
     SLinkedList<Integer> sll = new SLinkedList<>();
     System.out.println(sll.size());
     sll.addFirst(10);
+    sll.addFirst(20);
+    sll.addFirst(30);
+    //sll.addLast(40);
     System.out.println(sll.size());
+    //System.out.println(sll.getValue(3));
+    System.out.println(sll.getNode(3));
+
+    System.out.println(sll.removeFirst() + "이 삭제되었습니다.");
+    System.out.println("삭제 후 " + sll.size() + "개 남음");
+    System.out.println(sll.removeFirst() + "이 삭제되었습니다.");
+    System.out.println("삭제 후 " + sll.size() + "개 남음");
+    System.out.println(sll.removeFirst() + "이 삭제되었습니다.");
+    System.out.println("삭제 후 " + sll.size() + "개 남음");
   }
 }
